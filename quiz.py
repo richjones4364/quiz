df305fb57dfccb579395d6e5c6e135551ce0dfc3
Rich Jones <103132290+richjones4364@users.noreply.github.com>
2023-01-08 15:29:40 +0000

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -30,7 +30,7 @@ def make_request(quiz_type, num_q):
         "https://api.openai.com/v1/completions",
         headers={
             "Content-Type": "application/json",
-            "Authorization": f"Bearer {api_key}",
+            "Authorization": f"Bearer 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",
     },
     json={
         "model": "text-davinci-003",
